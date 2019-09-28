325d50642de5c7b9b57419549afc7abe9dce7acf
Juan David <jdalzatec@unal.edu.co>
2019-09-28 11:31:54 -0500

    Everything is worknig

--- functions.R
+++ functions.R
@@ -2,38 +2,38 @@ library("tidyverse")
 library("bibliometrix")
 library("igraph")
 
-read_isi_file < - function (isi_file) {
-    text < -readFiles(isi_file)
-    tos_dataframe < -convert2df(text, dbsource = "isi", format = "plaintext")
-    tos_dataframe$IDWOS < -rownames(tos_dataframe)
+read_isi_file <- function (isi_file) {
+    text <-readFiles(isi_file)
+    tos_dataframe <- convert2df(text, dbsource = "isi", format = "plaintext")
+    tos_dataframe$IDWOS <-rownames(tos_dataframe)
     return(tos_dataframe)
 }
 
-split_references < - function (tos_dataframe, separator) {
-    dataframe_splitted < - tos_dataframe %>% separate_rows(CR, sep = separator)
+split_references <- function (tos_dataframe, separator) {
+    dataframe_splitted <- tos_dataframe %>% separate_rows(CR, sep = separator)
     return(dataframe_splitted)
 }
 
 
-format_dataframe < - function (tos_dataframe) {
-    dataframe_formated < - tos_dataframe %>% mutate(IDWOS2 = paste(IDWOS, sep = ", ", paste("V", sep = "", VL), paste("P", sep = "", BP), paste("DOI ", sep = "", DI)))
+format_dataframe <- function (tos_dataframe) {
+    dataframe_formated <- tos_dataframe %>% mutate(IDWOS2 = paste(IDWOS, sep = ", ", paste("V", sep = "", VL), paste("P", sep = "", BP), paste("DOI ", sep = "", DI)))
     return(dataframe_formated)
 }
 
-graph_compute < - function (tos_dataframe) {
-    edges < -dataframe_formated[
-        !is.na(dataframe_formated$CR),
+graph_compute <- function (tos_dataframe) {
+    edges <- tos_dataframe[
+        !is.na(tos_dataframe$CR),
         c("IDWOS2", "CR")
     ]
-    tos_graph < -graph.data.frame(edges, directed = TRUE)
+    tos_graph <-graph.data.frame(edges, directed = TRUE)
     return(tos_graph)
 }
 
 
-clean_graph < - function (tos_graph) {
-    tos_degrees < -data.frame(node = V(graph = tos_graph)$name, indegree = degree(graph = tos_graph, mode = 'in'), outdegree = degree(graph = tos_graph, mode = 'out'), betweenness = betweenness(graph = tos_graph), directed = TRUE)
+clean_graph <- function (tos_graph) {
+    tos_degrees <- data.frame(node = V(graph = tos_graph)$name, indegree = degree(graph = tos_graph, mode = 'in'), outdegree = degree(graph = tos_graph, mode = 'out'), betweenness = betweenness(graph = tos_graph), directed = TRUE)
 
-    tos_cleaned < -subset(tos_degrees, !((tos_degrees$indegree == 1) & (tos_degrees$outdegree == 0)))
+    tos_cleaned <- subset(tos_degrees, !((tos_degrees$indegree == 1) & (tos_degrees$outdegree == 0)))
     return(tos_cleaned)
 
 }
--- main.R
+++ main.R
@@ -1,9 +1,11 @@
 source('functions.R')
 
-filename < - "f8417957-a805-489d-96e1-fc63ed9036e1.txt"
+filename <-  "f8417957-a805-489d-96e1-fc63ed9036e1.txt"
 
-tos_dataframe < -read_isi_file(filename)
-tos_dataframe < -split_references(tos_dataframe, "; ")
-tos_dataframe < -format_dataframe(tos_dataframe)
-tos_graph < -graph_compute(tos_dataframe)
-tos_graph < -clean_graph(tos_graph)
+tos_dataframe <- read_isi_file(filename)
+tos_dataframe <- split_references(tos_dataframe, "; ")
+tos_dataframe <- format_dataframe(tos_dataframe)
+tos_graph <- graph_compute(tos_dataframe)
+tos_graph <- clean_graph(tos_graph)
+
+print("Successfully !!!")
