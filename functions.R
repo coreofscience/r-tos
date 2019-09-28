3bad2df12b7bfea763eec92bed0c0c34b8f83007
jomdiazhe10 <jomdiazhe@unal.edu.co>
2019-09-28 14:35:47 +0000

    Dataframe Formated DOI organized

--- functions.R
+++ functions.R
@@ -13,3 +13,16 @@ split_references <- function(tos_dataframe, separator){
   dataframe_splitted  <- tos_dataframe %>% separate_rows(CR, sep = separator)
   return(dataframe_splitted)
 }
+
+
+format_dataframe <- function(tos_dataframe) {
+  dataframe_formated <-  tos_dataframe %>%
+    mutate(IDWOS2 = paste(IDWOS, sep = ", ",
+                          paste("V", sep = "", VL),
+                          paste("P", sep = "", BP),
+                          paste("DOI ", sep = "", DI)
+    )
+    )
+  return(dataframe_formated)
+}
+
