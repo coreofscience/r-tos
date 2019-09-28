25ca71b6cb68d828497f352a35d2e3dfc3dc7545
Teffy90 <eocampol@unal.edu.co>
2019-09-28 14:21:42 +0000

    Implement function to read isi files

--- functions.R
+++ functions.R
@@ -1,3 +1,10 @@
 library("tidyverse")
 library("bibliometrix")
 library("igraph")
+
+read_isi_file <- function(isi_file){
+  text <- readFiles(isi_file)
+  tos_dataframe <- convert2df(text, dbsource = "isi", format = "plaintext")
+  tos_dataframe$IDWOS <- rownames(tos_dataframe)
+  return(tos_dataframe)
+}
