719abec9c4ea6a0f244fc7f0fac8e0f03ebe3c1e
srobledog <director@coreofscience.com>
2020-04-16 19:09:40 +0000

    network metrics function was added

---
+++ network_metrics.R
@@ -0,0 +1,11 @@
+network.metrics <- function(graph) {
+  
+  network.metrics <- data.frame(
+    id = V(graph)$name,
+    indegree = degree(graph, mode = "in"),
+    outdegree = degree(graph, mode = "out"),
+    bet = betweenness(graph),
+    stringsAsFactors = FALSE
+  )
+  
+}
