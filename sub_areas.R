d15def66edacc181b0ac7d9e5ff25a977a69a0b2
srobledog <director@coreofscience.com>
2020-04-23 21:31:41 +0000

    addes some python lines to compare Blondel et al (2008) algotithm

--- sub_areas.R
+++ sub_areas.R
@@ -1,6 +1,10 @@
 sub_areas <- function(graph_tos) {
   
   library(reticulate)
-  py_run_string("import numpy as np")
+  py_install("networkx")
+  py_install("numpy")
+  py_run_string("import networkx as nx")
+  
+  py_run_string("my_python_array = np.array([2,4,6,8])")
   
 }
