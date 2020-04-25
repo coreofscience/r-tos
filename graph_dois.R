88dee5b799637bdc5a36ce2d1d50b33f92ccacf5
srobledog <director@coreofscience.com>
2020-04-25 22:59:50 +0000

    the code extracts dois from wos

--- graph_dois.R
+++ graph_dois.R
@@ -1,6 +1,6 @@
-wos_scopus <- function(wos_graph, scopus_graph) {
+graph_dois <- function(wos_graph, scopus_graph) {
   
-  edgelist_wos_tos <- 
+  edgelist_wos_id <- 
     as_tibble(as_edgelist(wos_graph)) %>% 
     rename(source = "V1",
            target = "V2") %>% 
@@ -9,32 +9,22 @@ wos_scopus <- function(wos_graph, scopus_graph) {
                                edgelist_wos$source),
            id_tos_target = sub("^(\\S*\\s+\\S+\\s+\\S+).*", # removing strings after second comma 
                                "\\1", 
-                               edgelist_wos$target)) %>% 
-    select(id_tos_source, 
-           id_tos_target)
+                               edgelist_wos$target)) 
   
+  list_wos_doi <- 
+    edgelist_wos_id %>%
+    set_names("source_target",
+              "source_target",
+              "id_tos_source_target",
+              "id_tos_source_target") 
   
-  edgelist_scopus_tos <- 
-    as_tibble(as_edgelist(scopus_graph)) %>% 
-    rename(id_tos_source = "V1",
-           id_tos_target = "V2") 
-  
-  edgelist_tos <- 
-    bind_rows(edgelist_wos_tos, 
-              edgelist_scopus_tos) %>% 
+  wos_source <- list_wos_doi[,c(1,3)]
+  wos_target <- list_wos_doi[,c(2,4)]
+  wos_source_target <- 
+    bind_rows(wos_source,
+              wos_target) %>% 
     distinct()
   
-  graph_tos <- 
-    graph.data.frame(edgelist_tos, 
-                     directed = TRUE) %>% 
-    simplify()
   
-  graph_tos_scopus <- 
-    delete.vertices(graph_tos, 
-                    which(degree(graph_tos, mode = "in") == 1 & 
-                            degree(graph_tos, mode = "out") == 0)) %>% 
-    giant.component()
-  
-  graph_tos_scopus
   
 }
