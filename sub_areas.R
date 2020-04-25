1809c17751bc6df75b75061bc3349f29d79a7bf9
srobledog <director@coreofscience.com>
2020-04-25 14:44:02 +0000

    The function subarea was updated, Blondel et al. (2008) shows the same results every time, so, it doesn't have the same problem as Gephi.

--- sub_areas.R
+++ sub_areas.R
@@ -1,12 +1,13 @@
-modularity <- function(graph_tos_scopus) {
+modularity <- function(graph_tos) {
   
-  graph_tos_scopus_undirected = as.undirected(graph_tos_scopus, 
-                                              mode = "each")
+  graph_tos_und = as.undirected(graph_tos,
+                                mode = "each")
   sub_areas = cluster_louvain(graph_tos_scopus_undirected)
-  graph_tos_scopus_und_subareas <- 
-    graph_tos_scopus_undirected %>% set_vertex_attr(name = "sub_area",
-                                                    value = membership(sub_areas)
-                                                    )
-  graph_tos_scopus_und_subareas
+  graph_tos_sub_areas <- 
+    graph_tos_und %>% 
+    set_vertex_attr(name = "sub_area",
+                    value = membership(sub_areas)
+                    )
+  graph_tos_sub_areas
 }
 
