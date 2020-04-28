e8af41961d20e0d2ac9857d661bac6d00b5dde03
srobledog <director@coreofscience.com>
2020-04-28 21:12:11 +0000

    function forest_wos was created. Create our first forest :-)

--- forest_wos.R
+++ forest_wos.R
@@ -3,6 +3,9 @@ forest_wos <- function(biblio_wos) {
   biblio_wos_ref <- 
     as_tibble(biblio_wos) %>% 
     separate_rows(CR, sep = ";") %>%  # the CR data is removed here, something to improve
-    group_by(ID_WOS) %>% 
-    nest()  
+    nest(data = CR) %>% 
+    rename("CR_FOREST" = data) %>% 
+    mutate(CR = biblio_wos$CR)
 }
+
+
