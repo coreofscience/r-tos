8e512645fb5717a39010805fe15b0311c5821174
srobledog <director@coreofscience.com>
2020-05-01 23:29:22 +0000

    Edge list is ended. Bibliometrix change the Scopus ID...

--- tos_scopus.R
+++ tos_scopus.R
@@ -2,10 +2,25 @@ tos_scopus <- function(biblio_scopus) {
   
   tos_scopus_ref <- 
     as_tibble(biblio_scopus) %>% 
+    mutate(ID_TOS = str_extract(SR, ".*,")) %>% 
     separate_rows(CR, sep = "; ") %>%  # the CR data is removed here, something to improve strsplit could be an option
     nest(data = CR) %>% 
-    rename("REFS_NESTED" = data,
-           "ID_SCOPUS" = SR) %>% 
-    mutate(CR = biblio_scopus$CR) %>% 
-    select(ID_SCOPUS, everything())
+    rename("REFS_NESTED" = data) %>% 
+    mutate(CR = biblio_scopus$CR,) %>% 
+    select(ID_TOS, everything())
+  
+  edge_list_scopus <- 
+    tos_scopus_ref %>% 
+    select(ID_SCOPUS, REFS_NESTED) %>% 
+    unnest(REFS_NESTED) %>%  
+    mutate(lastname = sub("\\., .*", "", CR),
+           lastname = sub(",", "", lastname),
+           lastname = sub("\\.", "", lastname),# extracting lastnames,
+           year = str_extract(CR, "\\(([0-9]{4})\\)"),
+           year = str_remove_all(year, "\\(|\\)")) %>%   # extracting year needs to be improved
+    filter(!grepl(pattern = "[():[:digit:]]", lastname),
+           str_length(year) == 4) %>% 
+    mutate(ID_SCOPUS = paste0(lastname, ", ", year, ","),
+           SR = str_trim(SR)) %>% 
+    select(SR, id_scopus)
 }
