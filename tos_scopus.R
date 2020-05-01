977325824d16a5015a40f58cb54ba49294faffb0
srobledog <director@coreofscience.com>
2020-05-01 22:03:53 +0000

    Convert biblio_scopus in tos_scopus_ref

--- tos_scopus.R
+++ tos_scopus.R
@@ -1,4 +1,11 @@
 tos_scopus <- function(biblio_scopus) {
   
-  
+  tos_scopus_ref <- 
+    as_tibble(biblio_scopus) %>% 
+    separate_rows(CR, sep = "; ") %>%  # the CR data is removed here, something to improve strsplit could be an option
+    nest(data = CR) %>% 
+    rename("REFS_NESTED" = data,
+           "ID_SCOPUS" = SR) %>% 
+    mutate(CR = biblio_scopus$CR) %>% 
+    select(ID_SCOPUS, everything())
 }
