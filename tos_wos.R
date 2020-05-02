1ea43df89402a9c7818aac4f7e4d9cdd55a6c386
srobledog <director@coreofscience.com>
2020-05-02 16:03:18 +0000

    A new column is added with nested references in tos format

--- tos_wos.R
+++ tos_wos.R
@@ -2,13 +2,31 @@ tos_wos <- function(biblio_wos) {
   
   tos_wos_ref <- 
     as_tibble(biblio_wos) %>% 
-    separate_rows(CR, sep = ";") %>%  # the CR data is removed here, something to improve strsplit could be an option
-    nest(data = CR) %>% 
-    rename("REFS_NESTED" = data) %>% 
+    separate_rows(CR, sep = ";") %>% # the CR data is removed here, something to improve strsplit could be an option
+    nest(CR_NESTED = CR) %>% 
+    mutate(CR = biblio_wos$CR) %>% 
+    mutate(REF_ID_TOS = sub("^(\\S*\\s+\\S+\\s+\\S+).*",
+                            "\\1", 
+                            CR)) %>% 
+    nest(REF_ID_TOS_NESTED = REF_ID_TOS) %>% 
     mutate(CR = biblio_wos$CR) %>% 
     select(ID_WOS, everything())
   
-  edge_list <- 
+  
+  edgelist_wos_tos <- 
+    as_tibble(as_edgelist(wos_graph)) %>% 
+    rename(source = "V1",
+           target = "V2") %>% 
+    mutate(id_tos_source = sub("^(\\S*\\s+\\S+\\s+\\S+).*", # removing strings after second comma 
+                               "\\1", 
+                               edgelist_wos$source),
+           id_tos_target = sub("^(\\S*\\s+\\S+\\s+\\S+).*", # removing strings after second comma 
+                               "\\1", 
+                               edgelist_wos$target)) %>% 
+    select(id_tos_source, 
+           id_tos_target)
+  
+  edge_list_tos <- 
     tos_wos_ref %>% 
     select(ID_WOS, REFS_NESTED) %>% 
     unnest(REFS_NESTED)
