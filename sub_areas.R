390803975e8d4b6ce317872d21317cb26d12b928
srobledog <director@coreofscience.com>
2020-05-04 23:58:25 +0000

    update variable names of clustering

--- sub_areas.R
+++ sub_areas.R
@@ -1,9 +1,9 @@
-modularity <- function(graph_tos) {
+modularity <- function(graph_wos_scopus) {
   
-  graph_tos_und = as.undirected(graph_tos,
+  graph_tos_und = as.undirected(graph_wos_scopus,
                                 mode = "each")
   sub_areas = cluster_louvain(graph_tos_und)
-  graph_tos_sub_areas <- 
+  graph_wos_scopus_sub_areas <- 
     graph_tos_und %>% 
     set_vertex_attr(name = "sub_area",
                     value = membership(sub_areas)
