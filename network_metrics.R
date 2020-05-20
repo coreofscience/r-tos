4ebf1c1951b78f1ee0171cc9e27f1eed51d643bb
marthavi <viviana.zuluagarojas@gmail.com>
2020-05-20 16:42:49 +0000

    Adding subarea to network metric

--- network_metrics.R
+++ network_metrics.R
@@ -5,6 +5,7 @@ network.metrics <- function(graph) {
     indegree = degree(graph, mode = "in"),
     outdegree = degree(graph, mode = "out"),
     bet = betweenness(graph),
+    subarea = V(graph)$sub_area,
     stringsAsFactors = FALSE
   )
    
